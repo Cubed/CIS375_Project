497c42611efa70dc0593776b86ac390f06508b25
Adam Said <adamsaid@umich.edu>
2024-11-14 17:51:24 -0500

    Fixed rate limit to only be 1 min

--- backend/main.js
+++ backend/main.js
@@ -30,7 +30,7 @@ app.use(morgan("combined"));
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 1 minute
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes.",
 });
