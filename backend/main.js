553991da3658f6045c24c71a9d2e019ee27a6d6f
jharwick23 <jharwick12@gmail.com>
2024-12-03 18:40:08 -0500

    changes

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load environment variables
 
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const { body, validationResult } = require("express-validator");
